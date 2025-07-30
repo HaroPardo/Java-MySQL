24c36d5e18faca71f320e7beff2387b5588a272a
Chules-programming <haropardocristina@gmail.com>
2025-07-30 23:46:37 +0200

    Initial commit

---
+++ src/main/java/com/movies/Java_MySQL/JavaMySqlApplication.java
@@ -0,0 +1,13 @@
+package com.movies.Java_MySQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMySqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/movies/Java_MySQL/JavaMySqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movies.Java_MySQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMySqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
